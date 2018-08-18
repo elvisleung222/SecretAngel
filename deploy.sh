bf960944e33ca9afba53577bb125719f6fba446e
Elvis <leung251v2@gmail.com>
2018-08-18 18:05:03 +0800

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 npm run build
-cd ./build
-git init
-git add -A
-git commit -m 'push by script'
-git remote add origin https://github.com/elvisleung222/elvisleung222.github.io.git
-git push -u origin master
+cd ./dist
+if [ "${PWD##*/}" == "dist" ];
+then
+    git init
+    git add -A
+    git commit -m 'push by script'
+    git remote add origin https://github.com/elvisleung222/elvisleung222.github.io.git
+    git push -u origin master
+fi
+cd ../
