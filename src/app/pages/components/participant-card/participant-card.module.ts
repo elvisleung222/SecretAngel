2f1a5a9af9743e54fc2d5a8750abd9bc7e1824b7
Ronald <ronaldchunyin@gmail.com>
2018-08-22 03:24:12 +0800

    Updated participant-card component style for reponsive

--- src/app/pages/components/participant-card/participant-card.module.ts
+++ src/app/pages/components/participant-card/participant-card.module.ts
@@ -3,14 +3,15 @@ import { ThemeModule } from '../../../@theme/theme.module';
 import { ParticipantCardComponent } from './participant-card.component';
 
 @NgModule({
-  imports: [
-    ThemeModule,
-  ],
-  declarations: [
-    ParticipantCardComponent
-  ],
-  exports: [
-    ParticipantCardComponent,
-  ]
+	imports: [
+		ThemeModule,
+	],
+	declarations: [
+		ParticipantCardComponent
+	],
+	exports: [
+		ParticipantCardComponent,
+	]
 })
-export class ParticipantCardModule { }
+export class ParticipantCardModule {
+}
