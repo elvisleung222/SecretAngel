c29809941b3c402092247c868a4c04990fcf3984
Elvis <leung251v2@gmail.com>
2018-08-22 13:59:34 +0800

    [WIP] patch to Firestore

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnDestroy} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
   selector: 'ngx-profile',
@@ -16,19 +21,51 @@ export class ProfileComponent implements OnDestroy {
   smoke: string;
   description: string;
 
-  constructor() {
+
+  constructor(private http:HttpClient) {
+    this.http.get('https://firestore.googleapis.com/v1beta1/projects/secretangel-dev/databases/(default)/documents/user/elvis').subscribe(
+      (data) => {
+        console.log(data);
+      }
+    )
   }
 
   submit(){
-    console.log(this.major);
-    console.log(this.college);
-    console.log(this.dob);
-    console.log(this.status_current);
-    console.log(this.status_times);
-    console.log(this.height);
-    console.log(this.weight);
-    console.log(this.smoke);
-    console.log(this.description);
+    this.http.patch('https://firestore.googleapis.com/v1beta1/projects/secretangel-dev/databases/(default)/documents/user/elvis', {
+      "fields": {
+        "major": {
+         "stringValue": String(this.major)
+        },
+        // "college": {
+        //  "stringValue": this.college
+        // },
+        // "dob": {
+        //  "stringValue": this.dob
+        // },
+        // "status_current": {
+        //  "stringValue": this.status_current
+        // },
+        // "status_times": {
+        //  "stringValue": this.status_times
+        // },
+        // "height": {
+        //  "stringValue": this.height
+        // },
+        // "weight": {
+        //  "stringValue": this.weight
+        // },
+        // "smoke": {
+        //  "stringValue": this.smoke
+        // },
+        // "description": {
+        //  "stringValue": this.description
+        // },
+       }
+    },httpOptions).subscribe(
+      (data) => {
+        console.log(data)
+      }
+    )
   }
   ngOnDestroy() {
   }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { ProfileComponent } from './profile.component';
 import { ParticipantCardModule } from '../components/participant-card/participant-card.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     ThemeModule,
-    ParticipantCardModule
+    ParticipantCardModule,
+    HttpClientModule
   ],
   declarations: [
     ProfileComponent
