bbff21ffca908b080e36610669dcf55116739734
Elvis <leung251v2@gmail.com>
2018-08-22 13:07:55 +0800

    [WIP] build profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -6,9 +6,30 @@ import {Component, OnDestroy} from '@angular/core';
 })
 export class ProfileComponent implements OnDestroy {
 
+  major: string;
+  college: string;
+  dob: string;
+  status_current: string;
+  status_times: string;
+  height: string;
+  weight: string;
+  smoke: string;
+  description: string;
+
   constructor() {
   }
 
+  submit(){
+    console.log(this.major);
+    console.log(this.college);
+    console.log(this.dob);
+    console.log(this.status_current);
+    console.log(this.status_times);
+    console.log(this.height);
+    console.log(this.weight);
+    console.log(this.smoke);
+    console.log(this.description);
+  }
   ngOnDestroy() {
   }
 }
